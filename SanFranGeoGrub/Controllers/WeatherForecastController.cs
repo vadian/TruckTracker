d16e0548990a70a7b66b92b1c3d8aeafafd7632f
Carl Middleton <dev@carlcodes.com>
2023-01-21 09:00:44 -0800

    allow anonymous

--- SanFranGeoGrub/Controllers/WeatherForecastController.cs
+++ SanFranGeoGrub/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SanFranGeoGrub.Controllers
 {
-    [Authorize]
+    [AllowAnonymous]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
