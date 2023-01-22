a89d879302b0c6d286f84530fc2f29d363421455
Carl Middleton <dev@carlcodes.com>
2023-01-21 16:36:44 -0800

    Add some tests and clean up functionality

--- SanFranGeoGrub.Tests/TruckTrackerTests.cs
+++ SanFranGeoGrub.Tests/TruckTrackerTests.cs
@@ -1,17 +1,62 @@
+using Socrata;
+
 namespace SanFranGeoGrub.Tests;
 
 public class CachedTruckTrackerTests
 {
-    private Mock<TruckDataSource> truckDataSource = new();
+    private Mock<ITruckDataSource> truckDataSource = new();
 
     [Fact]
-    public async Task TestGetData()
+    public async Task it_should_fetch_data_from_datasource()
     {
+        var mockTrucks = new List<FoodTruck>()
+        {
+            new FoodTruck() { __id = "one" },
+            new FoodTruck() { __id = "two" },
+        };
+
+        truckDataSource.Setup(x => x.GetAllFoodTrucks()).ReturnsAsync(mockTrucks);
         CachedTruckTracker CUT = new CachedTruckTracker(truckDataSource.Object);
 
         var trucks = await CUT.GetAllTrucks();
 
-        Assert.NotNull(trucks);
-        Assert.NotEmpty(trucks);
+        Assert.Equal(mockTrucks.Count, trucks.Count());
+    }
+
+    [Fact]
+    public async Task it_should_filter_by_name()
+    {
+        var mockTrucks = new List<FoodTruck>()
+        {
+            new () { Applicant = "Applicant One" },
+            new () { Applicant = "Applicant Two" },
+        };
+
+        truckDataSource.Setup(x => x.GetAllFoodTrucks()).ReturnsAsync(mockTrucks);
+        CachedTruckTracker CUT = new CachedTruckTracker(truckDataSource.Object);
+
+        var trucks = await CUT.GetTruckByName("ONE");
+
+        Assert.Single(trucks);
+        Assert.Equal("Applicant One", trucks.Single().Applicant);
+    }
+
+    [Fact]
+    public async Task it_should_filter_by_name_and_status()
+    {
+        var mockTrucks = new List<FoodTruck>()
+        {
+            new () { Applicant = "Valid One", Status = "INACTIVE" },
+            new () { Applicant = "Valid Two", Status = "ACTIVE"   },
+            new () { Applicant = "unknown",   Status = "INACTIVE" }
+        };
+
+        truckDataSource.Setup(x => x.GetAllFoodTrucks()).ReturnsAsync(mockTrucks);
+        CachedTruckTracker CUT = new CachedTruckTracker(truckDataSource.Object);
+
+        var trucks = await CUT.GetTruckByName("valid", "inactive");
+
+        Assert.Single(trucks);
+        Assert.Equal("Valid One", trucks.Single().Applicant);
     }
 }
---
+++ SanFranGeoGrub/Data/CachedTruckTracker.cs
@@ -0,0 +1,67 @@
+﻿using Socrata;
+
+namespace SanFranGeoGrub.Data;
+
+public class CachedTruckTracker : LiveTruckTracker
+{
+    private IEnumerable<FoodTruck>? _allTrucks;
+    
+    private static CachedTruckTracker? _instance;
+    public new static CachedTruckTracker Instance = _instance ??= new CachedTruckTracker(new TruckDataSource());
+
+    public async Task<IEnumerable<FoodTruck>> GetAllTrucks()
+    {
+        return _allTrucks ??= await _truckSource.GetAllFoodTrucks();
+    }
+
+    
+    public CachedTruckTracker(ITruckDataSource dataSource) : base(dataSource) { }
+
+    public override async Task<IEnumerable<FoodTruck>> GetTruckByName(string name, string? status = null)
+    {
+        var trucks = await GetAllTrucks();
+        trucks = trucks.Where(x => x.Applicant.ToLower().Contains(name.ToLower()));
+
+        if (status is not null)
+        {
+            trucks = trucks.Where(x => x.Status.ToLower().Equals(status.ToLower()));
+        }
+
+        return trucks;
+    }
+
+    public override async Task<IEnumerable<FoodTruck>> GetTruckByStreet(string street)
+    {
+        var trucks = await GetAllTrucks();
+        trucks = trucks
+            .Where(x => x.Location_address.ToLower().Contains(street.ToLower()));
+
+        return trucks;
+    }
+
+    public virtual async Task<IEnumerable<FoodTruck>> GetTrucksNear(decimal refLatitude, decimal refLongitude, bool includeAll = false)
+    {
+        var trucks = await GetAllTrucks();
+
+        double distance(decimal? lat, decimal? longitude)
+        {
+            if (lat is null or 0 || longitude is null or 0)
+            {
+                return double.MaxValue;
+            }
+            var latDiff = lat - refLatitude;
+            var longDiff = longitude - refLongitude;
+
+            return Math.Sqrt(Math.Pow((double)latDiff, 2) + Math.Pow((double)longDiff, 2));
+        }
+
+        if (!includeAll)
+        {
+            trucks = trucks.Where(x => x.Status == "APPROVED");
+        }
+
+        return trucks
+            .OrderBy(x => distance(x.Latitude, x.Longitude))
+            .Take(5);
+    }
+}
