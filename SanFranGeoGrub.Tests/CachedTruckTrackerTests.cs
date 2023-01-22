e2b7c63a0a13b6a4ebea5e81734fb218677dc79a
Carl Middleton <dev@carlcodes.com>
2023-01-21 17:33:38 -0800

    more tests

--- SanFranGeoGrub.Tests/CachedTruckTrackerTests.cs
+++ SanFranGeoGrub.Tests/CachedTruckTrackerTests.cs
@@ -42,6 +42,24 @@ public class CachedTruckTrackerTests
     }
 
     [Fact]
+    public async Task it_should_filter_by_street()
+    {
+        var mockTrucks = new List<FoodTruck>()
+        {
+            new () { Applicant = "Applicant One", Location_address = "Good" },
+            new () { Applicant = "Applicant Two", Location_address = "Bad" },
+        };
+
+        truckDataSource.Setup(x => x.GetAllFoodTrucks()).ReturnsAsync(mockTrucks);
+        CachedTruckTracker CUT = new CachedTruckTracker(truckDataSource.Object);
+
+        var trucks = await CUT.GetTruckByStreet("GOOD");
+
+        Assert.Single(trucks);
+        Assert.Equal("Applicant One", trucks.Single().Applicant);
+    }
+
+    [Fact]
     public async Task it_should_filter_by_name_and_status()
     {
         var mockTrucks = new List<FoodTruck>()
@@ -58,5 +76,27 @@ public class CachedTruckTrackerTests
 
         Assert.Single(trucks);
         Assert.Equal("Valid One", trucks.Single().Applicant);
+    }
+
+    [Fact]
+    public async Task it_should_filter_by_location()
+    {
+        var mockTrucks = new List<FoodTruck>()
+        {
+            new () { Applicant = "Valid", Status = "APPROVED", __id = "Correct", Latitude = -2, Longitude = -2},
+            new () { Applicant = "Valid", Status = "APPROVED", __id = "Correct", Latitude = 2, Longitude = 2 },
+            new () { Applicant = "Valid", Status = "APPROVED", __id = "Correct", Latitude = 3, Longitude = 3 },
+            new () { Applicant = "Valid", Status = "APPROVED", __id = "Correct", Latitude = -4, Longitude = 4 },
+            new () { Applicant = "Valid", Status = "APPROVED", __id = "Correct", Latitude = 1, Longitude = 1 },
+            new () { Applicant = "Valid", Status = "APPROVED", __id = "Incorrect", Latitude = 5, Longitude = -5 },
+        };
+
+        truckDataSource.Setup(x => x.GetAllFoodTrucks()).ReturnsAsync(mockTrucks);
+        CachedTruckTracker CUT = new CachedTruckTracker(truckDataSource.Object);
+
+        var trucks = await CUT.GetTrucksNear(0, 0);
+
+        Assert.Equal(5, trucks.Count());
+        Assert.True(trucks.All((x) => "Correct".Equals(x.__id)));
     }
 }
